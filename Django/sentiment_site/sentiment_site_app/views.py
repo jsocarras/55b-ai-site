a090bcc57183ec9d19b9aab9b1b8e31789b7bc82
JosephS <JRySocarras@gmail.com>
2018-09-23 21:09:10 -0500

    Django folder removed

--- Django/sentiment_site/sentiment_site_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create views here. Each view is a website element
-
-def index(request):
-    return HttpResponse("Hello World!")
